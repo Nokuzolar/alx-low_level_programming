9e113043aad6de1ca63e4469ecd9a523dd4fb307
Nokuzolar <nvunjani23@outlook.com>
2023-06-07 17:17:52 -0700

    101-wildcmp.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,19 @@
 #include "main.h"
-int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 /**
-* is_prime_number - says if an integer is a prime number or not
-* @n: number to evaluate
+* is_palindrome - checks if a string is a palindrome
+* @s: string to reverse
 *
-* Return: 1 if n is a prime number, 0 if not
+* Return: 1 if it is, 0 it's not
 */
-int is_prime_number(int n)
+int is_palindrome(char *s)
 {
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
+}
+/**
 * _strlen_recursion - returns the length of a string
 * @s: string to calculate the length of
 *
